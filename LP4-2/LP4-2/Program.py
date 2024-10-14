69ea69fbff7736460c23151b230863e3230a2db7
TaylorC6 <cahill.t1@stu.janesville.k12.wi.us>
2024-10-14 15:27:29 -0500

    10-14

---
+++ LP4-2/LP4-2/Program.py
@@ -0,0 +1,20 @@
+﻿Kilograms = int(input("Package Weight in Kilograms: "))
+Length = int(input("Package Length in Centimeters: "))
+Width = int(input("Package Width in Centimeters: "))
+Height = int(input("Package Height in Centimeters: "))
+heavy = 0.0
+large = 0.0
+if Kilograms > 27:
+	heavy = 1
+if (Height * Length * Width) > 100000:
+	large = 1
+if heavy == 1 and large == 0:
+	print("Too Heavy.")
+elif heavy == 0 and large == 1:
+	print("Too Large.")
+elif heavy == 1 and large == 1:
+	print("Too Heavy and Too Large.")
+else:
+	print("OK")
+
+input()
---
+++ LP4-3/LP4-3/Program.py
@@ -0,0 +1,23 @@
+﻿from math import floor
+eggs = int(input("# of Eggs: "))
+dozens = floor(eggs / 12)
+remainder = eggs % 12
+price = 0.0
+tcost = 0.0
+if dozens > 0 and dozens <= 4:
+	price = 0.50
+elif dozens > 4 and dozens <= 6:
+	price = 0.45
+elif dozens > 6 and dozens <= 11:
+	price = 0.40
+elif dozens > 11:
+	price = 0.35
+else:
+	print("! Invalid # of Dozens !")
+	
+
+tcost = (dozens * price) + (remainder * ((1.0 / 12) * price))
+
+print("Total Cost: $%.2f") % tcost
+
+input()
