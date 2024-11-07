bc9ef11144e4e9050e23d5bd4aca10dd860f7198
TaylorC6 <cahill.t1@stu.janesville.k12.wi.us>
2024-11-07 13:51:26 -0600

    11-6

---
+++ StringStuff/StringStuff/Program.py
@@ -0,0 +1,14 @@
+﻿thing = "HELLO world HERE is SOME text"
+print(thing.lower())
+print(thing.upper())
+print(thing.title())       # every world cap.
+print(thing.capitalize())  # cap. first letter only
+print(thing.isdigit())     # checks if # before crashing
+print(thing.find("world")) # find index # also see rfind, index, and rindex
+print(thing.count("E"))
+print(thing[::-1])         # reverse
+print(thing.split(" "))
+print("Hello" * 5)
+
+
+input()
