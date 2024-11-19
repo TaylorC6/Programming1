6ea3c8c56289708b49acbc3363b584658e8bf6ed
TaylorC6 <cahill.t1@stu.janesville.k12.wi.us>
2024-11-19 13:48:09 -0600

    11-18

---
+++ SlotMachine/MainForm.py
@@ -0,0 +1,309 @@
+﻿import System.Drawing
+import System.Windows.Forms
+
+from System.Drawing import *
+from System.Windows.Forms import *
+
+class MainForm(Form):
+    def __init__(self):
+        self.InitializeComponent()
+        self.num1 = 0 
+        self.num2 = 0
+        self.num3 = 0
+    
+    def InitializeComponent(self):
+        self._components = System.ComponentModel.Container()
+        resources = System.Resources.ResourceManager("SlotMachine.MainForm", System.Reflection.Assembly.GetEntryAssembly())
+        self._pictureBox1 = System.Windows.Forms.PictureBox()
+        self._pictureBox2 = System.Windows.Forms.PictureBox()
+        self._pictureBox3 = System.Windows.Forms.PictureBox()
+        self._button1 = System.Windows.Forms.Button()
+        self._button2 = System.Windows.Forms.Button()
+        self._label1 = System.Windows.Forms.Label()
+        self._textBox1 = System.Windows.Forms.TextBox()
+        self._label2 = System.Windows.Forms.Label()
+        self._label3 = System.Windows.Forms.Label()
+        self._progressBar1 = System.Windows.Forms.ProgressBar()
+        self._pictureBox5 = System.Windows.Forms.PictureBox()
+        self._pictureBox6 = System.Windows.Forms.PictureBox()
+        self._pictureBox7 = System.Windows.Forms.PictureBox()
+        self._pictureBox8 = System.Windows.Forms.PictureBox()
+        self._pictureBox9 = System.Windows.Forms.PictureBox()
+        self._pictureBox10 = System.Windows.Forms.PictureBox()
+        self._pictureBox11 = System.Windows.Forms.PictureBox()
+        self._pictureBox4 = System.Windows.Forms.PictureBox()
+        self._timer1 = System.Windows.Forms.Timer(self._components)
+        self._pictureBox1.BeginInit()
+        self._pictureBox2.BeginInit()
+        self._pictureBox3.BeginInit()
+        self._pictureBox5.BeginInit()
+        self._pictureBox6.BeginInit()
+        self._pictureBox7.BeginInit()
+        self._pictureBox8.BeginInit()
+        self._pictureBox9.BeginInit()
+        self._pictureBox10.BeginInit()
+        self._pictureBox11.BeginInit()
+        self._pictureBox4.BeginInit()
+        self.SuspendLayout()
+        # 
+        # pictureBox1
+        # 
+        self._pictureBox1.BackColor = System.Drawing.Color.White
+        self._pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        self._pictureBox1.Location = System.Drawing.Point(13, 13)
+        self._pictureBox1.Name = "pictureBox1"
+        self._pictureBox1.Size = System.Drawing.Size(212, 279)
+        self._pictureBox1.TabIndex = 0
+        self._pictureBox1.TabStop = False
+        # 
+        # pictureBox2
+        # 
+        self._pictureBox2.BackColor = System.Drawing.Color.White
+        self._pictureBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        self._pictureBox2.Location = System.Drawing.Point(248, 13)
+        self._pictureBox2.Name = "pictureBox2"
+        self._pictureBox2.Size = System.Drawing.Size(212, 279)
+        self._pictureBox2.TabIndex = 1
+        self._pictureBox2.TabStop = False
+        # 
+        # pictureBox3
+        # 
+        self._pictureBox3.BackColor = System.Drawing.Color.White
+        self._pictureBox3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        self._pictureBox3.Location = System.Drawing.Point(482, 12)
+        self._pictureBox3.Name = "pictureBox3"
+        self._pictureBox3.Size = System.Drawing.Size(212, 279)
+        self._pictureBox3.TabIndex = 2
+        self._pictureBox3.TabStop = False
+        # 
+        # button1
+        # 
+        self._button1.BackgroundImage = resources.GetObject("button1.BackgroundImage")
+        self._button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        self._button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        self._button1.Location = System.Drawing.Point(709, 13)
+        self._button1.Name = "button1"
+        self._button1.Size = System.Drawing.Size(249, 326)
+        self._button1.TabIndex = 3
+        self._button1.UseVisualStyleBackColor = True
+        self._button1.Click += self.Button1Click
+        # 
+        # button2
+        # 
+        self._button2.Font = System.Drawing.Font("Microsoft Sans Serif", 24, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
+        self._button2.Location = System.Drawing.Point(709, 355)
+        self._button2.Name = "button2"
+        self._button2.Size = System.Drawing.Size(249, 54)
+        self._button2.TabIndex = 4
+        self._button2.Text = "Pickpocket"
+        self._button2.UseVisualStyleBackColor = True
+        self._button2.Click += self.Button2Click
+        # 
+        # label1
+        # 
+        self._label1.BackColor = System.Drawing.Color.White
+        self._label1.Font = System.Drawing.Font("Microsoft Sans Serif", 24, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
+        self._label1.Location = System.Drawing.Point(709, 427)
+        self._label1.Name = "label1"
+        self._label1.Size = System.Drawing.Size(249, 38)
+        self._label1.TabIndex = 5
+        self._label1.Text = "Bet:"
+        # 
+        # textBox1
+        # 
+        self._textBox1.Font = System.Drawing.Font("Microsoft Sans Serif", 18, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
+        self._textBox1.Location = System.Drawing.Point(780, 429)
+        self._textBox1.Name = "textBox1"
+        self._textBox1.Size = System.Drawing.Size(165, 35)
+        self._textBox1.TabIndex = 7
+        # 
+        # label2
+        # 
+        self._label2.BackColor = System.Drawing.Color.WhiteSmoke
+        self._label2.Font = System.Drawing.Font("Microsoft Sans Serif", 24, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
+        self._label2.Location = System.Drawing.Point(806, 481)
+        self._label2.Name = "label2"
+        self._label2.Size = System.Drawing.Size(139, 34)
+        self._label2.TabIndex = 8
+        self._label2.Text = "100"
+        self._label2.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
+        self._label2.Click += self.Label2Click
+        # 
+        # label3
+        # 
+        self._label3.BackColor = System.Drawing.Color.Transparent
+        self._label3.Font = System.Drawing.Font("Microsoft Sans Serif", 24, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
+        self._label3.Location = System.Drawing.Point(709, 477)
+        self._label3.Name = "label3"
+        self._label3.Size = System.Drawing.Size(249, 38)
+        self._label3.TabIndex = 9
+        self._label3.Text = "Cash:"
+        # 
+        # progressBar1
+        # 
+        self._progressBar1.Location = System.Drawing.Point(709, 536)
+        self._progressBar1.Maximum = 15000
+        self._progressBar1.Name = "progressBar1"
+        self._progressBar1.Size = System.Drawing.Size(249, 43)
+        self._progressBar1.TabIndex = 11
+        # 
+        # pictureBox5
+        # 
+        self._pictureBox5.BackColor = System.Drawing.Color.DimGray
+        self._pictureBox5.BackgroundImage = resources.GetObject("pictureBox5.BackgroundImage")
+        self._pictureBox5.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        self._pictureBox5.Location = System.Drawing.Point(22, 322)
+        self._pictureBox5.Name = "pictureBox5"
+        self._pictureBox5.Size = System.Drawing.Size(69, 63)
+        self._pictureBox5.TabIndex = 13
+        self._pictureBox5.TabStop = False
+        self._pictureBox5.Visible = False
+        # 
+        # pictureBox6
+        # 
+        self._pictureBox6.BackColor = System.Drawing.Color.DimGray
+        self._pictureBox6.BackgroundImage = resources.GetObject("pictureBox6.BackgroundImage")
+        self._pictureBox6.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        self._pictureBox6.Location = System.Drawing.Point(117, 322)
+        self._pictureBox6.Name = "pictureBox6"
+        self._pictureBox6.Size = System.Drawing.Size(69, 63)
+        self._pictureBox6.TabIndex = 14
+        self._pictureBox6.TabStop = False
+        self._pictureBox6.Visible = False
+        # 
+        # pictureBox7
+        # 
+        self._pictureBox7.BackColor = System.Drawing.Color.DimGray
+        self._pictureBox7.BackgroundImage = resources.GetObject("pictureBox7.BackgroundImage")
+        self._pictureBox7.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        self._pictureBox7.Location = System.Drawing.Point(310, 322)
+        self._pictureBox7.Name = "pictureBox7"
+        self._pictureBox7.Size = System.Drawing.Size(69, 63)
+        self._pictureBox7.TabIndex = 16
+        self._pictureBox7.TabStop = False
+        self._pictureBox7.Visible = False
+        # 
+        # pictureBox8
+        # 
+        self._pictureBox8.BackColor = System.Drawing.Color.DimGray
+        self._pictureBox8.BackgroundImage = resources.GetObject("pictureBox8.BackgroundImage")
+        self._pictureBox8.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        self._pictureBox8.Location = System.Drawing.Point(215, 322)
+        self._pictureBox8.Name = "pictureBox8"
+        self._pictureBox8.Size = System.Drawing.Size(69, 63)
+        self._pictureBox8.TabIndex = 15
+        self._pictureBox8.TabStop = False
+        self._pictureBox8.Visible = False
+        # 
+        # pictureBox9
+        # 
+        self._pictureBox9.BackColor = System.Drawing.Color.DimGray
+        self._pictureBox9.BackgroundImage = resources.GetObject("pictureBox9.BackgroundImage")
+        self._pictureBox9.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        self._pictureBox9.Location = System.Drawing.Point(410, 322)
+        self._pictureBox9.Name = "pictureBox9"
+        self._pictureBox9.Size = System.Drawing.Size(69, 63)
+        self._pictureBox9.TabIndex = 17
+        self._pictureBox9.TabStop = False
+        self._pictureBox9.Visible = False
+        # 
+        # pictureBox10
+        # 
+        self._pictureBox10.BackColor = System.Drawing.Color.DimGray
+        self._pictureBox10.BackgroundImage = resources.GetObject("pictureBox10.BackgroundImage")
+        self._pictureBox10.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        self._pictureBox10.Location = System.Drawing.Point(22, 468)
+        self._pictureBox10.Name = "pictureBox10"
+        self._pictureBox10.Size = System.Drawing.Size(69, 99)
+        self._pictureBox10.TabIndex = 18
+        self._pictureBox10.TabStop = False
+        self._pictureBox10.Visible = False
+        # 
+        # pictureBox11
+        # 
+        self._pictureBox11.BackColor = System.Drawing.Color.DimGray
+        self._pictureBox11.BackgroundImage = resources.GetObject("pictureBox11.BackgroundImage")
+        self._pictureBox11.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        self._pictureBox11.Location = System.Drawing.Point(97, 468)
+        self._pictureBox11.Name = "pictureBox11"
+        self._pictureBox11.Size = System.Drawing.Size(69, 99)
+        self._pictureBox11.TabIndex = 19
+        self._pictureBox11.TabStop = False
+        self._pictureBox11.Visible = False
+        # 
+        # pictureBox4
+        # 
+        self._pictureBox4.Image = resources.GetObject("pictureBox4.Image")
+        self._pictureBox4.Location = System.Drawing.Point(13, 311)
+        self._pictureBox4.Name = "pictureBox4"
+        self._pictureBox4.Size = System.Drawing.Size(690, 283)
+        self._pictureBox4.TabIndex = 20
+        self._pictureBox4.TabStop = False
+        # 
+        # MainForm
+        # 
+        self.BackColor = System.Drawing.Color.Maroon
+        self.ClientSize = System.Drawing.Size(972, 655)
+        self.Controls.Add(self._pictureBox11)
+        self.Controls.Add(self._pictureBox10)
+        self.Controls.Add(self._pictureBox9)
+        self.Controls.Add(self._pictureBox7)
+        self.Controls.Add(self._pictureBox8)
+        self.Controls.Add(self._pictureBox6)
+        self.Controls.Add(self._pictureBox5)
+        self.Controls.Add(self._label2)
+        self.Controls.Add(self._progressBar1)
+        self.Controls.Add(self._label3)
+        self.Controls.Add(self._textBox1)
+        self.Controls.Add(self._label1)
+        self.Controls.Add(self._button2)
+        self.Controls.Add(self._button1)
+        self.Controls.Add(self._pictureBox3)
+        self.Controls.Add(self._pictureBox2)
+        self.Controls.Add(self._pictureBox1)
+        self.Controls.Add(self._pictureBox4)
+        self.Name = "MainForm"
+        self.Text = "SlotMachine"
+        self._pictureBox1.EndInit()
+        self._pictureBox2.EndInit()
+        self._pictureBox3.EndInit()
+        self._pictureBox5.EndInit()
+        self._pictureBox6.EndInit()
+        self._pictureBox7.EndInit()
+        self._pictureBox8.EndInit()
+        self._pictureBox9.EndInit()
+        self._pictureBox10.EndInit()
+        self._pictureBox11.EndInit()
+        self._pictureBox4.EndInit()
+        self.ResumeLayout(False)
+        self.PerformLayout()
+
+
+    def Label2Click(self, sender, e):
+        pass
+
+    def Button2Click(self, sender, e):
+        rand = System.Random()
+        money = rand.Next(1,51)
+        if money > 25:
+            MessageBox.Show("YOU FAILED TO STEAL MONEY!")
+        else:
+            cmoney = float(self._label2.Text)
+            self._label2.Text = str(round(cmoney + money, 2))
+        
+        pass
+
+    def Button1Click(self, sender, e):
+        im1 = self._pictureBox5.BackgroundImage
+        im2 = self._pictureBox6.BackgroundImage
+        im3 = self._pictureBox7.BackgroundImage
+        im4 = self._pictureBox8.BackgroundImage
+        im5 = self._pictureBox9.BackgroundImage
+        levOff = self._pictureBox10.BackgroundImage
+        levOn = self._pictureBox11.BackgroundImage
+        rnd = System.Random()
+        num1 = 0
+        num2 = 0
+        num3 = 0
+        # Copy above into timerTick
+        pass
---
+++ prog_247/MainForm.py
@@ -0,0 +1,155 @@
+﻿import System.Drawing
+import System.Windows.Forms
+
+from System.Drawing import *
+from System.Windows.Forms import *
+
+class MainForm(Form):
+    def __init__(self):
+        self.InitializeComponent()
+    
+    def InitializeComponent(self):
+        self._radioButton1 = System.Windows.Forms.RadioButton()
+        self._radioButton2 = System.Windows.Forms.RadioButton()
+        self._radioButton3 = System.Windows.Forms.RadioButton()
+        self._checkBox1 = System.Windows.Forms.CheckBox()
+        self._checkBox2 = System.Windows.Forms.CheckBox()
+        self._checkBox3 = System.Windows.Forms.CheckBox()
+        self._button1 = System.Windows.Forms.Button()
+        self._button2 = System.Windows.Forms.Button()
+        self.SuspendLayout()
+        # 
+        # radioButton1
+        # 
+        self._radioButton1.BackColor = System.Drawing.Color.FromArgb(255, 128, 128)
+        self._radioButton1.Font = System.Drawing.Font("Microsoft Sans Serif", 20.25, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
+        self._radioButton1.Location = System.Drawing.Point(22, 37)
+        self._radioButton1.Name = "radioButton1"
+        self._radioButton1.Size = System.Drawing.Size(153, 42)
+        self._radioButton1.TabIndex = 0
+        self._radioButton1.TabStop = True
+        self._radioButton1.Text = "Choice 1"
+        self._radioButton1.UseVisualStyleBackColor = False
+        # 
+        # radioButton2
+        # 
+        self._radioButton2.BackColor = System.Drawing.Color.FromArgb(255, 128, 128)
+        self._radioButton2.Font = System.Drawing.Font("Microsoft Sans Serif", 20.25, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
+        self._radioButton2.Location = System.Drawing.Point(22, 113)
+        self._radioButton2.Name = "radioButton2"
+        self._radioButton2.Size = System.Drawing.Size(153, 42)
+        self._radioButton2.TabIndex = 1
+        self._radioButton2.TabStop = True
+        self._radioButton2.Text = "Choice 2"
+        self._radioButton2.UseVisualStyleBackColor = False
+        # 
+        # radioButton3
+        # 
+        self._radioButton3.BackColor = System.Drawing.Color.FromArgb(255, 128, 128)
+        self._radioButton3.Font = System.Drawing.Font("Microsoft Sans Serif", 20.25, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
+        self._radioButton3.Location = System.Drawing.Point(22, 186)
+        self._radioButton3.Name = "radioButton3"
+        self._radioButton3.Size = System.Drawing.Size(153, 42)
+        self._radioButton3.TabIndex = 2
+        self._radioButton3.TabStop = True
+        self._radioButton3.Text = "Choice 3"
+        self._radioButton3.UseVisualStyleBackColor = False
+        # 
+        # checkBox1
+        # 
+        self._checkBox1.BackColor = System.Drawing.Color.FromArgb(255, 128, 128)
+        self._checkBox1.Font = System.Drawing.Font("Microsoft Sans Serif", 20.25, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
+        self._checkBox1.Location = System.Drawing.Point(272, 37)
+        self._checkBox1.Name = "checkBox1"
+        self._checkBox1.Size = System.Drawing.Size(153, 42)
+        self._checkBox1.TabIndex = 3
+        self._checkBox1.Text = "Choice 4"
+        self._checkBox1.UseVisualStyleBackColor = False
+        # 
+        # checkBox2
+        # 
+        self._checkBox2.BackColor = System.Drawing.Color.FromArgb(255, 128, 128)
+        self._checkBox2.Font = System.Drawing.Font("Microsoft Sans Serif", 20.25, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
+        self._checkBox2.Location = System.Drawing.Point(272, 114)
+        self._checkBox2.Name = "checkBox2"
+        self._checkBox2.Size = System.Drawing.Size(153, 42)
+        self._checkBox2.TabIndex = 4
+        self._checkBox2.Text = "Choice 5"
+        self._checkBox2.UseVisualStyleBackColor = False
+        # 
+        # checkBox3
+        # 
+        self._checkBox3.BackColor = System.Drawing.Color.FromArgb(255, 128, 128)
+        self._checkBox3.Font = System.Drawing.Font("Microsoft Sans Serif", 20.25, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
+        self._checkBox3.Location = System.Drawing.Point(272, 187)
+        self._checkBox3.Name = "checkBox3"
+        self._checkBox3.Size = System.Drawing.Size(153, 42)
+        self._checkBox3.TabIndex = 5
+        self._checkBox3.Text = "Choice 6"
+        self._checkBox3.UseVisualStyleBackColor = False
+        # 
+        # button1
+        # 
+        self._button1.BackColor = System.Drawing.Color.FromArgb(192, 0, 0)
+        self._button1.Font = System.Drawing.Font("Microsoft Sans Serif", 24, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
+        self._button1.Location = System.Drawing.Point(22, 264)
+        self._button1.Name = "button1"
+        self._button1.Size = System.Drawing.Size(181, 57)
+        self._button1.TabIndex = 6
+        self._button1.Text = "Calculate"
+        self._button1.UseVisualStyleBackColor = False
+        self._button1.Click += self.Button1Click
+        # 
+        # button2
+        # 
+        self._button2.BackColor = System.Drawing.Color.FromArgb(192, 0, 0)
+        self._button2.Font = System.Drawing.Font("Microsoft Sans Serif", 24, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
+        self._button2.Location = System.Drawing.Point(243, 264)
+        self._button2.Name = "button2"
+        self._button2.Size = System.Drawing.Size(182, 57)
+        self._button2.TabIndex = 7
+        self._button2.Text = "Exit"
+        self._button2.UseVisualStyleBackColor = False
+        self._button2.Click += self.Button2Click
+        # 
+        # MainForm
+        # 
+        self.BackColor = System.Drawing.Color.FromArgb(255, 192, 192)
+        self.ClientSize = System.Drawing.Size(453, 352)
+        self.Controls.Add(self._button2)
+        self.Controls.Add(self._button1)
+        self.Controls.Add(self._checkBox3)
+        self.Controls.Add(self._checkBox2)
+        self.Controls.Add(self._checkBox1)
+        self.Controls.Add(self._radioButton3)
+        self.Controls.Add(self._radioButton2)
+        self.Controls.Add(self._radioButton1)
+        self.Name = "MainForm"
+        self.Text = "prog_247"
+        self.ResumeLayout(False)
+
+
+    def Button1Click(self, sender, e):
+        
+        stri = ""
+        
+        if self._radioButton1.Checked:
+            stri = "Choice 1"
+        elif self._radioButton2.Checked:
+            stri = "Choice 2"
+        elif self._radioButton3.Checked:
+            stri = "Choice 3"
+        if self._checkBox1.Checked:
+            stri += " and Choice 4"
+        if self._checkBox2.Checked:
+            stri += " and Choice 5"
+        if self._checkBox3.Checked:
+            stri += " and Choice 6"
+        
+        MessageBox.Show(str(stri) + " Selected!")
+        
+        
+            
+
+    def Button2Click(self, sender, e):
+        Application.Exit()
